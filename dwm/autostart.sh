c586ad05b1c81831bf20a39872ffb87593d54c66
leozamboni <leonardonunes169@gmail.com>
2022-06-22 13:58:02 -0300

    update

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 while true; do
-		xsetroot -name "$(printf "💾 total: %s, used %s of %s | 💿 %s" ${$(free -h -t | sed 1,3d):1} $(df -h /home --output=pcent | sed 1d)) | 📅 $(date '+%H:%M:%S %A %B %d/%m/%y')"
+		xsetroot -name "$(printf "💾 %s of %s | 💿 %s" ${$(free -h -t | sed 1,3d):2} $(df -h /home --output=pcent | sed 1d)) | 📅 $(date '+%H:%M:%S %A %B %d/%m/%y')"
 		sleep 1s
 done &
